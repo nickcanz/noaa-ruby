f688f2b464b571aafbe82f9f16310454c7f9ae3c
Nicholas Canzoneri <ac3522@gmail.com>
2011-06-01 23:06:14 -0400

    Added weather station data from NOAA and start of ruby script to import said weather station data into mongo db

---
+++ data/importing_data.rb
@@ -0,0 +1,31 @@
+require 'mongo'
+require 'csv'
+
+def convert_latlon(deg_str)
+  # DD-MMH = degs, mins, hemisphere(N/S)
+  arr = deg_str.split("-")
+  deg = arr[0].to_f
+  mins = arr[1].slice(0, arr[1].length-1).to_f * 60
+  hemi_str = arr[1].slice(-1)
+  hemisphere = (hemi_str == "N" || hemi_str == "E") ? 1 : -1
+
+  deg_dec = deg + (mins / 3600)
+  deg_dec * hemisphere
+end
+
+#db = Mongo::Connection.new.db("mydb")
+#stations = db["weatherStations"]
+#
+#CSV.foreach("weather_stations.txt", { :col_sep => ';'}) do |row|
+#  station = {
+#    "ICAOIndicator" => row[0],
+#    "placeName"     => row[3],
+#    "loc" => {
+#      "lat" => row[7],
+#      "lon" => row[8]
+#    }
+#  }
+#  p row
+#end
+
+
