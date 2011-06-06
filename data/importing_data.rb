824501367c2327e59e327be5549692d67b3a0965
Nicholas Canzoneri <ac3522@gmail.com>
2011-06-05 23:11:54 -0400

    Inserting data into mongo. Updated bad weather stations csv

--- data/importing_data.rb
+++ data/importing_data.rb
@@ -2,21 +2,33 @@ require 'mongo'
 require 'csv'
 
 def convert_latlon(deg_str)
-  # DD-MMH = degs, mins, hemisphere(N/S)
   arr = deg_str.split("-")
   deg = arr[0].to_f
-  mins = arr[1].slice(0, arr[1].length-1).to_f * 60
-  hemi_str = arr[1].slice(-1)
+  
+  mins = 0
+  secs = 0
+  hemi_str = ''
+
+  # DD-MMH = degs, mins, hemisphere
+  # or 
+  # DD-MM-SSH = degs, mins, secs, hemisphere
+  if(arr.length == 2) 
+    mins = arr[1].slice(0, arr[1].length-1).to_f * 60
+    hemi_str = arr[1].slice(-1)
+  elsif (arr.length == 3) 
+    mins = arr[1].to_f
+    secs = arr[2].slice(0, arr[2].length-1).to_f
+    hemi_str = arr[2].slice(-1)
+  end
+
   hemisphere = (hemi_str == "N" || hemi_str == "E") ? 1 : -1
 
-  deg_dec = deg + (mins / 3600)
+  deg_dec = deg + ((mins+secs) / 3600)
   deg_dec * hemisphere
 end
 
-db = Mongo::Connection.new.db("mydb")
+db = Mongo::Connection.new.db("myapp")
 stations = db["weatherStations"]
-
-count = 1
 
 CSV.foreach("weather_stations.txt", { :col_sep => ';'}) do |row|
   station = {
@@ -27,6 +39,5 @@ CSV.foreach("weather_stations.txt", { :col_sep => ';'}) do |row|
       "lon" => convert_latlon(row[8])
     }
   }
-  count += 1
-  p count
+stations.insert(station)
 end
