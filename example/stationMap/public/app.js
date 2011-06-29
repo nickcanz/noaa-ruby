fea55fefa4cbc232cebbc329c2625448c4ac6f6b
Nicholas Canzoneri <ac3522@gmail.com>
2011-06-29 00:08:39 -0400

    Adding javascript to throw the 100 weather stations nearest to center onto the map

--- example/stationMap/public/app.js
+++ example/stationMap/public/app.js
@@ -6,4 +6,32 @@ $(document).ready(function () {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), opts);
+
+  var markers = [];
+
+  google.maps.event.addListener(map, 'dragend', function () {
+    var center = map.getCenter();
+    $.each(markers, function (idx, marker) {
+      marker.setMap(null);
+    });
+    $.ajax({
+      url: 'stations/' + center.lat() + '/' + center.lng(),
+      success: function (stations) {
+        markers = [];
+        $.each(stations, function (idx, station) {
+          var loc= station.location;
+          markers.push(
+            new google.maps.Marker({
+              position: new google.maps.LatLng(loc.lat, loc.lon),
+              map: map,
+              title: station.name
+            })
+          );
+        });
+      },
+      dataType: 'json'
+    });
+  });
+
+  google.maps.event.trigger(map, 'dragend');
 });
